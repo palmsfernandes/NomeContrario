4de08735f0bf8a5bce5052bd13de19bd34d8249c
Vinicius Fernandes <vinicius.fernandes@bunzlbrasil.protcapad.com.br>
2020-06-02 17:57:45 -0300

    Novo

---
+++ NomeContrario/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NomeContrario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+                                    
+            Console.Write("Digite seu nome aqui: ");
+            nome = Console.ReadLine().ToUpper();
+            
+            string invertido = new string(nome.Reverse().ToArray());
+                                    
+            Console.WriteLine("Seu nome invertido é: " + invertido);
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
